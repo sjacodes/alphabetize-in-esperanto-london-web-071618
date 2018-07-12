37b93a4c7ce029b5d5a86a7c51208cc39c6b1b3e
Sarah Jacob <smjacob91@gmail.com>
2018-07-12 15:25:14 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,5 @@
 def alphabetize(arr)
-  #make the alphabet an array of strings, breaking out each element (aphabet letter)
-  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z" ]
+  esperanto_alphabet = 
   arr.sort_by do |word|
     word.split("").map do |alphabet_letter|
        
