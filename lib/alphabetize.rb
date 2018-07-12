61bb0ed46e0c756892722eb763f8bc68e83d5966
Sarah Jacob <smjacob91@gmail.com>
2018-07-12 15:27:02 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,7 @@
 def alphabetize(arr)
+  #separate alphabet into into strings of individual letters
   esperanto_alphabet =  "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split(//)
+  #sort
   arr.sort_by do |word|
     word.split("").map do |alphabet_letter|
        
